6341865b4b46650603fbae4721ef72c951881d8a
shimaaadel0448-123 <shimaaadel0448@gmail.com>
2025-08-13 14:21:38 +0300

    Add Angular project files

---
+++ src/Components/projects/projects.ts
@@ -0,0 +1,39 @@
+// featured-projects.component.ts
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-projects',
+
+  imports:[CommonModule],
+  templateUrl: './projects.html',
+  styleUrls: ['./projects.css']
+})
+export class Projects {
+  projects = [
+    {
+      title: 'Responsive Landing Page',
+      description: 'A modern, responsive landing page built with HTML, CSS, JavaScript, and Bootstrap. Features smooth scrolling, interactive elements, and mobile-first design.',
+      image: '/student.PNG',
+      technologies: ['angular', 'Bootstrap CSS', 'TypeScript'],
+      liveDemo: '#',
+      codeLink: '#'
+    },
+    {
+      title: 'E-commerce React Website',
+      description: 'A fully functional e-commerce website built with angular and Bootstrap CSS. Includes product catalog, shopping cart, and responsive design.',
+      image: '/ecommerce.PNG',
+      technologies: ['angular', 'Bootstrap CSS', 'TypeScript',"rjrx"],
+      liveDemo: '#',
+      codeLink: '#'
+    },
+    {
+      title: 'Portfolio Showcase',
+      description: 'A personal portfolio website showcasing my skills and projects. Built with modern web technologies and responsive design principles.',
+      image: '/port.PNG',
+      technologies: ['angular', 'Bootstrap CSS', 'TypeScript'],
+      liveDemo: '#',
+      codeLink: '#'
+    }
+  ];
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from '../Components/navbar/navbar';
+import { Hero } from '../Components/hero/hero';
+import { About } from '../Components/about/about';
+import { Projects } from '../Components/projects/projects';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar,Hero,About,Projects],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
